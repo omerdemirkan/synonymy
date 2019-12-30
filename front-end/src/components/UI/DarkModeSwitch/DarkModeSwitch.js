6770aefed1a28562f5ced74e6da232bc5e79b0a9
Omer Demirkan <omerfarukpiano@gmail.com>
2019-12-30 15:36:18 -0800

    add icons to darkModeSwitch with altering opacities

--- front-end/src/components/UI/DarkModeSwitch/DarkModeSwitch.js
+++ front-end/src/components/UI/DarkModeSwitch/DarkModeSwitch.js
@@ -1,19 +1,38 @@
 
 import React from 'react';
+import classes from './DarkModeSwitch.module.css';
 
+// Redux
 import { connect } from 'react-redux';
 import * as actionTypes from '../../../store/actions/actionTypes';
 
+// Material UI
 import Switch from '@material-ui/core/Switch';
+import NightsStayRoundedIcon from '@material-ui/icons/NightsStayRounded';
+import WbSunnyRoundedIcon from '@material-ui/icons/WbSunnyRounded';
 
 const darkModeSwitch = props => {
     return <span>
+        <WbSunnyRoundedIcon 
+        fontSize="small" 
+        className={classes.SunIcon}
+        style={props.darkMode ? {opacity: '.5'}: null}
+        />
+
         <Switch
         checked={props.darkMode}
         onChange={props.toggleDarkMode}
         color="default"
+        className={classes.Switch}
         style={props.darkMode ? {color: '#444'} : {color: '#eee'}}
         />
+
+        <NightsStayRoundedIcon 
+        fontSize="small" 
+        className={classes.MoonIcon}
+        style={!props.darkMode ? {opacity: '.5'}: null}
+        />
+
     </span>
 }
 
