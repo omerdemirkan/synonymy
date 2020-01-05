d7b1518913d4306797ca8726c4bd51f70cac363d
Omer Demirkan <omerfarukpiano@gmail.com>
2020-01-05 14:11:19 -0800

    switch from moby to thesaurus package

--- routes/synonyms.js
+++ routes/synonyms.js
@@ -1,11 +1,12 @@
 const router = require('express').Router()
-const moby = require('moby');
+var thesaurus = require("thesaurus");
 
 router.post('/', (req, res) => {
     const list = req.body.list;
     if (list && list.length < 30 && list.length > 0) {
         list.forEach(object => {
-            const synonyms = moby.search(object.word)
+            // const synonyms = moby.search(object.word)
+            const synonyms = thesaurus.find(object.word);
             if (synonyms && synonyms.length > 0) {
                 object.synonyms = synonyms;
             } else {
