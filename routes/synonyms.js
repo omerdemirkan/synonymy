e3f50d0c488b452faf92317168d1a5a19f5c2c33
Omer Demirkan <omerfarukpiano@gmail.com>
2020-01-13 15:53:21 -0800

    add rate limiter on synonyms route

--- routes/synonyms.js
+++ routes/synonyms.js
@@ -1,7 +1,13 @@
 const router = require('express').Router()
 var thesaurus = require("thesaurus");
+const rateLimit = require("express-rate-limit");
 
-router.post('/', (req, res) => {
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 50 // limit each IP to 50 requests per windowMs
+});
+
+router.post('/', limiter, (req, res) => {
     const wordList = req.body.list;
     let synonymsList = {}
     if (wordList && wordList.length < 30 && wordList.length > 0) {
